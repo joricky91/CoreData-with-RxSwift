b7b5e9a2b1b204cf339800e1b4d527e75470b458
joricky91 <jonathanricky91@gmail.com>
2023-02-09 19:31:34 +0700

    feat: creating table view

--- CoreDataRxSwift/View/ViewController.swift
+++ CoreDataRxSwift/View/ViewController.swift
@@ -12,9 +12,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemTeal
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
+        setupContraints()
     }
 
-
+    lazy var tableView: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    func setupContraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            tableView.heightAnchor.constraint(equalTo: safeArea.heightAnchor),
+        ])
+    }
+    
 }
 
